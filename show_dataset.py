3f52aa2d5055f2a059e2e0a1c745fe930989218a
Seb <trakiss@gmail.com>
2015-11-13 21:39:58 +0100

    add headers in show_dataset

--- show_dataset.py
+++ show_dataset.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
-dataset = pd.read_csv('data/raw/dataset.csv');
+dataset = pd.read_csv('data/raw/dataset.csv')
+dataset.columns = ["Map", "Visitor Team", "VisitorPts", "Home Team", "HomePts"]
+
 print dataset.ix[:5]
