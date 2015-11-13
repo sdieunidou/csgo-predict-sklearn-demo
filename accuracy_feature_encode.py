de3a74784bdc0fdd42aa1cfce9be50b4e4c8fda4
Seb <trakiss@gmail.com>
2015-11-14 00:52:17 +0100

    update

--- accuracy_feature_encode.py
+++ accuracy_feature_encode.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
+from sklearn.grid_search import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.cross_validation import cross_val_score
 from sklearn.preprocessing import LabelEncoder
 from collections import defaultdict
+from sklearn.ensemble import RandomForestClassifier
 
 dataset = pd.read_csv('data/raw/dataset.csv')
 dataset.columns = ["Map", "Team1", "Team1Pts", "Team2", "Team2Pts"]
@@ -90,3 +92,18 @@ X_teams_expanded = dataset[["Team1", "Team2", "Map", "Team1LastWin", "Team2LastW
 clf = DecisionTreeClassifier()
 scores = cross_val_score(clf, X_teams_expanded, y_true, scoring='accuracy')
 print("Accuracy: {0:.1f}%".format(np.mean(scores) * 100))
+
+parameter_space = {
+ "max_features": [3, 8, 'auto'],
+ "n_estimators": [1000,],
+ "criterion": ["gini", "entropy"],
+ "min_samples_leaf": [3, 4, 5, 6, 7, 8, 9, 10],
+}
+clf = RandomForestClassifier()
+scores = cross_val_score(clf, X_teams_expanded, y_true, scoring='accuracy')
+print("Accuracy: {0:.1f}%".format(np.mean(scores) * 100))
+
+grid = GridSearchCV(clf, parameter_space)
+grid.fit(X_teams_expanded, y_true)
+print("Accuracy: {0:.1f}%".format(grid.best_score_ * 100))
+print(grid.best_estimator_)
